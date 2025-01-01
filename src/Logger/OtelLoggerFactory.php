483ec16af850a4fac4e8145fd74c9fe18480b1d3
Imranul Sazzad <imranulsazzad@gmail.com>
2025-01-01 11:02:53 +0600

    Add files via upload

--- src/Logger/OtelLoggerFactory.php
+++ src/Logger/OtelLoggerFactory.php
@@ -9,16 +9,36 @@ use OpenTelemetry\SDK\Logs\Processor\BatchLogRecordProcessor;
 use OpenTelemetry\SDK\Resource\ResourceInfoFactory;
 use OpenTelemetry\API\Common\Time\SystemClock;
 use OpenTelemetry\Contrib\Grpc\GrpcTransportFactory;
+use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
 use OpenTelemetry\Contrib\Otlp\LogsExporter;
 
 class OtelLoggerFactory
 {
     public function __invoke(array $config): OtelLogger
     {
-        $endpoint = config('opentelemetry.endpoint') . '/opentelemetry.proto.collector.logs.v1.LogsService/Export';
+        $endpoint = config('opentelemetry.endpoint');
+        $protocol = config('opentelemetry.protocol');
 
-        $transport = (new GrpcTransportFactory())->create($endpoint, 'application/x-protobuf');
-        $logExporter = new LogsExporter($transport);
+        if (!filter_var($endpoint, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException('Invalid OTEL_EXPORTER_OTLP_ENDPOINT URL provided.');
+        }
+
+        $logExporter = match ($protocol) {
+            'grpc' => new LogsExporter(
+                (new GrpcTransportFactory())->create(
+                    $endpoint . '/opentelemetry.proto.collector.logs.v1.LogsService/Export',
+                    'application/x-protobuf'
+                )
+            ),
+            'http' => new LogsExporter(
+                (new OtlpHttpTransportFactory())->create(
+                    $endpoint . '/v1/logs',
+                    'application/json'
+                )
+            ),
+            default => throw new \InvalidArgumentException("Unsupported protocol: $protocol"),
+        };
+
         $logProcessor = new BatchLogRecordProcessor($logExporter, new SystemClock(), 2048, 1000000000, 512);
         $attributesFactory = new AttributesFactory();
         $instrumentationScopeFactory = new InstrumentationScopeFactory($attributesFactory);
